108f5e084d3cc323a78c498e45898c33ed1ede17
PoojaKotur <133103238+PoojaKotur@users.noreply.github.com>
2024-02-28 08:12:14 +0100

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "nn-silver-2024" # Replace with your actual S3 bucket name
+    bucket = "pooja-mario" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
